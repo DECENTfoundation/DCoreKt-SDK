4c768d2f914c3510d94f35c70aee1b2e6924d8a8
Juraj Škorvánek <juraj8skorvanek@gmail.com>
2018-06-13 09:54:09 +0200

    Added submit content operation

--- src/main/java/ch/decent/sdk/model/PricePerRegion.kt
+++ src/main/java/ch/decent/sdk/model/PricePerRegion.kt
@@ -1,17 +1,17 @@
 package ch.decent.sdk.model
 
 import ch.decent.sdk.net.serialization.ByteSerializable
+import ch.decent.sdk.net.serialization.bytes
 import com.google.common.primitives.Bytes
 import com.google.gson.annotations.SerializedName
 
-data class KeyParts(
-    @SerializedName("C1") val keyC1: PubKey,
-    @SerializedName("D1") val keyD1: PubKey
+data class RegionalPrice(
+    @SerializedName("price") val prices: AssetAmount,
+    @SerializedName("region") val region: Int
 ) : ByteSerializable {
   override val bytes: ByteArray
     get() = Bytes.concat(
-        keyC1.bytes,
-        keyD1.bytes
+        region.bytes(),
+        prices.bytes
     )
-
 }
