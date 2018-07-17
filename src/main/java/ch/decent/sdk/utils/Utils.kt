35efdc951874c1e6e8a8c2c9da1f9a8499eaa62c
Juraj Skorvanek <js@decent.ch>
2018-07-17 09:49:03 +0200

    Added documentation comment for AES128 CBC decrypt method

--- src/main/java/ch/decent/sdk/utils/Utils.kt
+++ src/main/java/ch/decent/sdk/utils/Utils.kt
@@ -78,6 +78,32 @@ fun encryptAes(key: ByteArray, clear: ByteArray): ByteArray {
   return outBuf
 }
 
+/**
+ * decrypt AES 128bit CBC PKCS5 Padding
+ *
+ * @param initialVector initialVector
+ * @param encryptedText encryptedText
+ * @param key encryption key
+ *
+ * @throws IllegalStateException - if this cipher is in a wrong state (e.g., has not been initialized)
+ *
+ * @throws IllegalBlockSizeException - if this cipher is a block cipher, no padding has been requested
+ * (only in encryption mode), and the total input length of the data processed by this cipher is not a multiple of block size;
+ * or if this encryption algorithm is unable to process the input data provided.
+ *
+ * @throws BadPaddingException - if this cipher is in decryption mode, and (un)padding has been requested,
+ * but the decrypted data is not bounded by the appropriate padding bytes
+ *
+ * @throws AEADBadTagException - if this cipher is decrypting in an AEAD mode (such as GCM/CCM),
+ * and the received authentication tag does not match the calculated value
+ *
+ * @throws InvalidKeyException - if the public key in the given certificate is inappropriate for initializing this cipher,
+ * or this cipher requires algorithm parameters that cannot be determined from the public key in the given certificate,
+ * or the keysize of the public key in the given certificate has a keysize that exceeds the maximum allowable keysize
+ * (as determined by the configured jurisdiction policy files).
+ *
+ * @return encrypted text
+ */
 fun decryptAes128(initialVector: ByteArray, encryptedText: ByteArray, key: ByteArray): String =
   Cipher.getInstance("AES/CBC/PKCS5Padding").let {
       it.init(Cipher.DECRYPT_MODE, SecretKeySpec(key, "AES"), IvParameterSpec(initialVector))
