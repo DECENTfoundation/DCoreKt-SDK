88d81163f196eae2469ecb4f70889f9f1db9478f
Lukas Vyletel <lukas@decent.ch>
2018-09-10 15:18:27 +0200

    Fix missing trx_in_block parameter in OperationHistory

--- src/main/java/ch/decent/sdk/model/OperationHistory.kt
+++ src/main/java/ch/decent/sdk/model/OperationHistory.kt
@@ -8,6 +8,7 @@ data class OperationHistory(
     @SerializedName("op") val operation: BaseOperation,
     @SerializedName("result") val result: JsonArray,
     @SerializedName("block_num") val blockNum: Long,
+    @SerializedName("trx_in_block") val trxInBlock: Long,
     @SerializedName("op_in_trx") val operationNumInTrx: Long,
     @SerializedName("virtual_op") val virtualOperation: Long
 )
