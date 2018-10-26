d5fa08f1107b9e0972ab40d486cf93451c93e682
Marian Vanderka <vanderka.marian@gmail.com>
2018-10-26 16:26:42 +0200

    cleanup aes

--- library/src/main/java/ch/decent/sdk/utils/Utils.kt
+++ library/src/main/java/ch/decent/sdk/utils/Utils.kt
@@ -12,6 +12,7 @@ import org.bouncycastle.crypto.modes.CBCBlockCipher
 import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher
 import org.bouncycastle.crypto.params.KeyParameter
 import org.bouncycastle.crypto.params.ParametersWithIV
+import org.bouncycastle.jcajce.provider.asymmetric.ec.KeyFactorySpi
 import java.math.BigInteger
 import java.nio.charset.Charset
 import java.security.MessageDigest
@@ -53,12 +54,14 @@ fun decryptAesWithChecksum(key: ByteArray, encrypted: ByteArray): String {
   }
 }
 
-fun decryptAes(key: ByteArray, encrypted: ByteArray): ByteArray {
-  val cipher = PaddedBufferedBlockCipher(CBCBlockCipher(AESEngine()))
-  val ivBytes = key.copyOfRange(32, 32 + 16)
-  val sks = key.copyOfRange(0, 32)
+fun decryptAes(keyWithIv: ByteArray, encrypted: ByteArray): ByteArray {
+  val ivBytes = keyWithIv.copyOfRange(32, 32 + 16)
+  val sks = keyWithIv.copyOfRange(0, 32)
+  return decryptAes(ivBytes, sks, encrypted)
+}
 
-  cipher.init(false, ParametersWithIV(KeyParameter(sks), ivBytes))
+fun decryptAes(iv: ByteArray, key: ByteArray, encrypted: ByteArray): ByteArray {
+  val cipher = createCipher(false, iv, key)
   val clear = ByteArray(cipher.getOutputSize(encrypted.size))
   var len = cipher.processBytes(encrypted, 0, encrypted.size, clear, 0)
   len += cipher.doFinal(clear, len)
@@ -66,12 +69,14 @@ fun decryptAes(key: ByteArray, encrypted: ByteArray): ByteArray {
   return clear.copyOf(len)
 }
 
-fun encryptAes(key: ByteArray, clear: ByteArray): ByteArray {
-  val cipher = PaddedBufferedBlockCipher(CBCBlockCipher(AESEngine()))
-  val ivBytes = key.copyOfRange(32, 32 + 16)
-  val sks = key.copyOfRange(0, 32)
+fun encryptAes(keyWithIv: ByteArray, clear: ByteArray): ByteArray {
+  val ivBytes = keyWithIv.copyOfRange(32, 32 + 16)
+  val sks = keyWithIv.copyOfRange(0, 32)
+  return encryptAes(ivBytes, sks, clear)
+}
 
-  cipher.init(true, ParametersWithIV(KeyParameter(sks), ivBytes))
+fun encryptAes(iv: ByteArray, key: ByteArray, clear: ByteArray): ByteArray {
+  val cipher = createCipher(true, iv, key)
   val outBuf = ByteArray(cipher.getOutputSize(clear.size))
 
   var processed = cipher.processBytes(clear, 0, clear.size, outBuf, 0)
@@ -81,33 +86,13 @@ fun encryptAes(key: ByteArray, clear: ByteArray): ByteArray {
 }
 
 /**
- * decrypt AES 128bit CBC PKCS5 Padding
+ * create AES cipher with a fixed block size of 128 bits and available key sizes 128/192/256 bits
  *
- * @param initialVector initialVector
- * @param encryptedText encryptedText
- * @param key encryption key
- *
- * @throws IllegalStateException - if this cipher is in a wrong state (e.g., has not been initialized)
- *
- * @throws IllegalBlockSizeException - if this cipher is a block cipher, no padding has been requested
- * (only in encryption mode), and the total input length of the data processed by this cipher is not a multiple of block size;
- * or if this encryption algorithm is unable to process the input data provided.
- *
- * @throws BadPaddingException - if this cipher is in decryption mode, and (un)padding has been requested,
- * but the decrypted data is not bounded by the appropriate padding bytes
- *
- * @throws AEADBadTagException - if this cipher is decrypting in an AEAD mode (such as GCM/CCM),
- * and the received authentication tag does not match the calculated value
- *
- * @throws InvalidKeyException - if the public key in the given certificate is inappropriate for initializing this cipher,
- * or this cipher requires algorithm parameters that cannot be determined from the public key in the given certificate,
- * or the keysize of the public key in the given certificate has a keysize that exceeds the maximum allowable keysize
- * (as determined by the configured jurisdiction policy files).
- *
- * @return encrypted text
+ * @param forEncryption if true the cipher is initialised for encryption, if false for decryption
+ * @param iv initial vector, 128 bits
+ * @param key encryption key, 128/192/256 bits
  */
-fun decryptAes128(initialVector: ByteArray, encryptedText: ByteArray, key: ByteArray): String =
-  Cipher.getInstance("AES/CBC/PKCS5Padding").let {
-      it.init(Cipher.DECRYPT_MODE, SecretKeySpec(key, "AES"), IvParameterSpec(initialVector))
-      String(it.doFinal(encryptedText))
-  }
+fun createCipher(forEncryption: Boolean, iv: ByteArray, key: ByteArray) =
+    PaddedBufferedBlockCipher(CBCBlockCipher(AESEngine())).apply {
+      init(forEncryption, ParametersWithIV(KeyParameter(key), iv))
+    }
