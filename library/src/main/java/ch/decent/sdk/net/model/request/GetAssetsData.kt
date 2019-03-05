498e96824ee265e4ee1df2c824fbe517e34fd9ab
Juraj Skorvanek <js@decent.ch>
2019-03-05 15:03:11 +0100

    Asset conversion

--- library/src/main/java/ch/decent/sdk/net/model/request/GetAssetsData.kt
+++ library/src/main/java/ch/decent/sdk/net/model/request/GetAssetsData.kt
@@ -6,12 +6,12 @@ import ch.decent.sdk.model.ObjectType
 import com.google.gson.reflect.TypeToken
 
 internal class GetAssetsData(
-    contentIds: List<ChainObject>
+    assetDataIds: List<ChainObject>
 ) : GetObjects<List<AssetData>>(
-    contentIds.map { it },
+    assetDataIds,
     TypeToken.getParameterized(List::class.java, AssetData::class.java).type
 ) {
   init {
-    require(contentIds.all { it.objectType == ObjectType.ASSET_DYNAMIC_DATA }) { "not a valid asset dynamic data object id" }
+    require(assetDataIds.all { it.objectType == ObjectType.ASSET_DYNAMIC_DATA }) { "not a valid asset dynamic data object id" }
   }
 }
