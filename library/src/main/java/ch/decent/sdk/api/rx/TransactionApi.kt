4277e76b6905182b031c6dc0542fbca5bcf5528e
Peter Vanderka <peter.vanderka@decent.ch>
2019-10-04 13:37:52 +0200

    Removed get recent transaction by ID method based on decision of DCore.

--- library/src/main/java/ch/decent/sdk/api/rx/TransactionApi.kt
+++ library/src/main/java/ch/decent/sdk/api/rx/TransactionApi.kt
@@ -4,24 +4,12 @@ package ch.decent.sdk.api.rx
 
 import ch.decent.sdk.crypto.Address
 import ch.decent.sdk.crypto.Credentials
-import ch.decent.sdk.model.AccountObjectId
-import ch.decent.sdk.model.Fee
-import ch.decent.sdk.model.OpWrapper
-import ch.decent.sdk.model.ProcessedTransaction
-import ch.decent.sdk.model.Proposal
-import ch.decent.sdk.model.ProposalObjectId
-import ch.decent.sdk.model.Transaction
-import ch.decent.sdk.model.TransactionConfirmation
+import ch.decent.sdk.model.*
 import ch.decent.sdk.model.operation.BaseOperation
 import ch.decent.sdk.model.operation.ProposalCreateOperation
 import ch.decent.sdk.model.operation.ProposalDeleteOperation
 import ch.decent.sdk.model.operation.ProposalUpdateOperation
-import ch.decent.sdk.net.model.request.GetProposalById
-import ch.decent.sdk.net.model.request.GetProposedTransactions
-import ch.decent.sdk.net.model.request.GetRecentTransactionById
-import ch.decent.sdk.net.model.request.GetTransaction
-import ch.decent.sdk.net.model.request.GetTransactionById
-import ch.decent.sdk.net.model.request.GetTransactionHex
+import ch.decent.sdk.net.model.request.*
 import io.reactivex.Single
 import org.threeten.bp.Duration
 import org.threeten.bp.LocalDateTime
@@ -63,16 +51,6 @@ class TransactionApi internal constructor(api: DCoreApi) : BaseApi(api) {
   fun getAllProposed(ids: List<ProposalObjectId>): Single<List<Proposal>> = GetProposalById(ids).toRequest()
 
   fun getProposed(id: ProposalObjectId): Single<Proposal> = getAllProposed(listOf(id)).map { it.single() }
-
-  /**
-   * If the transaction has not expired, this method will return the transaction for the given ID or it will return [ch.decent.sdk.exception.ObjectNotFoundException].
-   * Just because it is not known does not mean it wasn't included in the DCore. The ID can be retrieved from [Transaction] or [TransactionConfirmation] objects.
-   *
-   * @param trxId transaction id
-   *
-   * @return a transaction if found, [ch.decent.sdk.exception.ObjectNotFoundException] otherwise
-   */
-  fun getRecent(trxId: String): Single<ProcessedTransaction> = GetRecentTransactionById(trxId).toRequest()
 
   /**
    * This method will return the transaction for the given ID or it will return [ch.decent.sdk.exception.ObjectNotFoundException].
