96791a39e7b0eee2df6319821c10a12b3ad4ce54
Peter Vanderka <peter.vanderka@decent.ch>
2019-11-07 14:16:30 +0100

    DSDK-748 Removed set_subscribe_callback and cancel_all_subscriptions methods from CallbackApi. It will be removed from DCore.

--- library/src/main/java/ch/decent/sdk/api/rx/CallbackApi.kt
+++ library/src/main/java/ch/decent/sdk/api/rx/CallbackApi.kt
@@ -2,22 +2,14 @@
 
 package ch.decent.sdk.api.rx
 
-import ch.decent.sdk.net.model.request.CancelAllSubscriptions
 import ch.decent.sdk.net.model.request.SetBlockAppliedCallback
 import ch.decent.sdk.net.model.request.SetContentUpdateCallback
 import ch.decent.sdk.net.model.request.SetPendingTransactionCallback
-import ch.decent.sdk.net.model.request.SetSubscribeCallback
 import io.reactivex.Flowable
-import io.reactivex.Single
 
 // todo how to cancel callbacks ?
 // cancel.. works only on setSubscribeCallback
 class CallbackApi internal constructor(api: DCoreApi) : BaseApi(api) {
-
-  /**
-   * Stop receiving any notifications. This unsubscribes from all subscribed objects ([onGlobal] and [AccountApi.getFullAccounts]).
-   */
-  fun cancelAll(): Single<Unit> = CancelAllSubscriptions.toRequest()
 
   /**
    * Receive new block notifications. Cannot be cancelled.
@@ -35,11 +27,4 @@ class CallbackApi internal constructor(api: DCoreApi) : BaseApi(api) {
    * Receive notifications on pending transactions. Cannot be cancelled.
    */
   fun onPendingTransaction(): Flowable<Unit> = SetPendingTransactionCallback.callbacks()
-
-  /**
-   * Subscribe to callbacks. Can be cancelled. with [cancelAll].
-   *
-   * @param clearFilter clear current subscriptions created with [AccountApi.getFullAccounts]
-   */
-  fun onGlobal(clearFilter: Boolean): Flowable<Unit> = SetSubscribeCallback(clearFilter).callbacks()
 }
