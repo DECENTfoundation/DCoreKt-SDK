6aef63a19d82b6222f967850d05f5a159f702e10
Juraj Škorvánek <juraj8skorvanek@gmail.com>
2019-11-07 16:01:26 +0100

    Added timestamp and transaction)id parameter to BalanceChange object (#109)

--- library/src/main/java/ch/decent/sdk/model/BalanceChange.kt
+++ library/src/main/java/ch/decent/sdk/model/BalanceChange.kt
@@ -1,11 +1,14 @@
 package ch.decent.sdk.model
 
 import com.google.gson.annotations.SerializedName
+import org.threeten.bp.LocalDateTime
 
 data class BalanceChange(
     @SerializedName("hist_object") val operation: OperationHistory,
     @SerializedName("balance") val balance: Balance,
-    @SerializedName("fee") val fee: AssetAmount
+    @SerializedName("fee") val fee: AssetAmount,
+    @SerializedName("timestamp") val timestamp: LocalDateTime,
+    @SerializedName("transaction_id") val transactionId: String
 )
 
 // ??
